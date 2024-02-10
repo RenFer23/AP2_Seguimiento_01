e84eec9bf3ed43e2cbd948b0f20e000cacc762b4
Renzo Fernando <renzofernando.md@gmail.com>
2024-02-10 09:11:07 -0500

    them.

--- src/test/java/co/icesi/edu/model/InfrastructureDepartamentTest.java
+++ src/test/java/co/icesi/edu/model/InfrastructureDepartamentTest.java
@@ -15,6 +15,56 @@ public class InfrastructureDepartamentTest {
         // Agregar un cartel al departamento de infraestructura
         department.addBillboard(10.0, 5.0, true, "Coca-Cola");
     }
+
+    @Test
+    public void testPromedioAltura() throws Exception {
+        InfrastructureDepartament department = new InfrastructureDepartament();
+
+        double promedioAltura = department.promedioAltura();
+        Assert.assertEquals(303.4, promedioAltura, 000.1);
+    }
+
+    @Test
+    public void testPromedioAnchos() throws Exception {
+        InfrastructureDepartament department = new InfrastructureDepartament();
+
+        double promedioAnchos = department.promedioAnchos();
+        Assert.assertEquals(792.2, promedioAnchos, 000.1);
+    }
+
+    @Test
+    public void testPromedioAreas() throws Exception {
+        InfrastructureDepartament department = new InfrastructureDepartament();
+
+        double promedioAreas = department.promedioAreas();
+        Assert.assertEquals(236091.0, promedioAreas, 000.1);
+    }
+
+    @Test
+    public void testContadorDeActivos() throws Exception {
+        InfrastructureDepartament department = new InfrastructureDepartament();
+
+        String contadorDeActivos = department.contadorDeActivos();
+        String supestoResultado = "Hay 3 verdaderos y 2 falsos.";
+
+        //se tenian que compara comparar cadenas de texto en forma de varibles
+        Assert.assertEquals(supestoResultado, contadorDeActivos);
+    }
+
+    @Test
+    public void testNombresConRepeticiones() throws Exception {
+        InfrastructureDepartament department = new InfrastructureDepartament();
+
+        String resultado = department.nombresConRepeticiones();
+        String supestoResultado = " - Auctor Quis Tristique Corp.: 1 veces\n" +
+                " - Hendrerit Institute: 2 veces\n" +
+                " - Risus Morbi LLC: 2 veces\n";
+        Assert.assertEquals(supestoResultado, resultado);
+    }
+
+
+
+
 }
 
 
